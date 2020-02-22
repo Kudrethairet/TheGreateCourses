a6f7aad21b0915a4a24566579b365374ac080685
kudret hairat <kudrethairet@gmail.com>
2020-02-21 19:27:48 -0500

    Jenkins Test

--- src/test/java/com/greatcourse/tests/smokeTest.java
+++ src/test/java/com/greatcourse/tests/smokeTest.java
@@ -12,7 +12,7 @@ public class smokeTest extends BasePage {
 	
 	@Test
 	public void VerifiedHomePage() {
-		assertEquals(hp.getTitle(), "Online Courses & Lectures for Home Study and Lifelong Learning");
+		assertEquals(hp.getTitle(), "Onine Courses & Lectures for Home Study and Lifelong Learning");
 		hp.click_Have_a_Priority_Code_Elem();
 		assertEquals(hp.get_what_Are_Priority_codes_elem(), hp.returnExpectedCodeText());
 		//hp.verify_Courses_menu();
